250c5301c74406330e0696b8d532c3690245d704
Tomgxz <thomaslee10.TL@gmail.com>
2023-06-15 16:42:42 +0100

    Stuff

--- Error.py
+++ Error.py
@@ -2,3 +2,14 @@ class UnderflowError(Exception):
     def __init__(self,message="The stack is empty"):
         self.message=message
         super().__init__(message)
+
+# deprecated
+
+#class SizeOverflowError(Exception):
+    #def __init__(self,requestedSize,message="The requested stack size is too large (maximum 10)"):
+        #self.message=message
+        #self.requestedSize=requestedSize
+        #super().__init__(message)
+
+    #def __str__(self):
+        #return f'{self.requestedSize} -> {self.message}'
--- Stack.py
+++ Stack.py
@@ -1,12 +1,13 @@
 from Error import UnderflowError
 
 class Stack(object):
-    def __init__(self,size):
+    def __init__(self,size,initPopulate=True):
         self.object=[]
         self.size=size
 
-        for i in range(self.size):
-            self.object.append(None)
+        if initPopulate:
+            for i in range(self.size):
+                self.object.append(None)
 
         self.bottom=0
         self.top=0
@@ -35,3 +36,51 @@ class Stack(object):
 
     full=property(isFull,None)
     empty=property(isEmpty,None)
+
+"""
+
+Create a program which emulates a processor Stack, the program should…
+
+Have a maximum stack size of 10 items
+
+Initially generate 5 items to populate the stack
+
+After this, at random intervals up to 10 seconds, add a new item to the stack
+
+Each item should have a random total task time of up to 7 seconds
+
+The processor should work on the “top” task in the stack and if a job is complete it can leave, if a new task comes
+the task can be paused – it does not need to be reset
+
+To do this completely will require you to use threading to manage the task addition and processing.
+
+"""
+
+from random import randint
+from threading import Thread
+import time
+
+class ProcessorStack(Stack):
+    def __init__(self):
+        super().__init__(10,initPopulate=False)
+        self.threads=[]
+
+        self.newProcessThread=Thread(target=self.newProcessRepeat)
+        self.threads.append(self.newProcessThread)
+        self.newProcessThread.start()
+
+        self.currentTaskThreads=[]
+
+    def newProcessRepeat(self):
+        while True:
+            time.sleep(randint(1,10))
+            if not self.isFull(): self.push(randint(1,7))
+
+    def queueTask(self):
+        currentTask=self.object[self.top-1]
+        
+
+    def task(self,time,active=True):
+        for i in range(time):
+            if active:
+                time.sleep(1)
